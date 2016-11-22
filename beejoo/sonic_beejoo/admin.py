3ccb7a139e0bcf829d0f24b5e301a5160f1be8a7
Gich <psyh.psyh@gmail.com>
2016-11-22 18:31:45 +0300

    added simple admin v1. and views

--- beejoo/sonic_beejoo/admin.py
+++ beejoo/sonic_beejoo/admin.py
@@ -20,10 +20,20 @@ class GoodsAdmin(admin.ModelAdmin):
     list_display = ('id',
                     'title',
                     'category',
+                    'get_designs',
+                    'get_colors',
                     'short_description',
                     'price',
                     'amount',
                     'is_displayed')
+
+    def get_designs(self, obj):
+        return '\n'.join([d.name for d in obj.design_types.all()])
+
+    def get_colors(self, obj):
+        return '\n'.join([c.name for c in obj.colors.all()])
+
+
 
 admin.site.register(Color, ColorAdmin)
 admin.site.register(Category, CategoryAdmin)
